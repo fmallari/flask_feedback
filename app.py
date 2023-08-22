90f7ddfe7ed2d39da87c67285e525f25102a2bc3
fmallari <fc.mallari@gmail.com>
2023-08-22 07:41:46 -0700

    'update postgres

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///flask-feedback"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = 'abc123'
+app.config['SECRET_KEY'] = 'jaekae'
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 connect_db(app)
